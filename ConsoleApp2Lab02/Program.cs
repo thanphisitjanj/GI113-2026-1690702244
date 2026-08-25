62239a090e306520c56b717a8f91ce895366ff7f
thanphisitjanj <thanphisit.janj@bumail.net>
2026-08-25 10:39:31 +0700

    add lab02

---
+++ ConsoleApp2Lab02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp2Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
